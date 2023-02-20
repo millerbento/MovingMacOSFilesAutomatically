153d1682cfeb068251df9f95e3a252ad4d324aac
Miller Bento <millermbento@gmail.com>
2023-02-21 10:37:40 +1300

    Better error handling when moving files
    Making it not raise exceptions if the destination directory already exists

--- MoveFoldersToServer.py
+++ MoveFoldersToServer.py
@@ -1,70 +1,53 @@
-from datetime import datetime
 import os
 from pathlib import Path
-import shutil
+from shutil import move
+import sys
+import traceback
 
 def MoveFiles(source, destination):
     try:
-        # #Creating folder name
-        # now = datetime.now()
-        # folderdatename = now.strftime('%Y-%m-%d')
-        # dt_string = now.strftime('%d%m%Y_%H_%M')
-
-        # #Create a folder with today's date 
-        # try:
-        #         destination = destination + folderdatename
-        #         os.mkdir(destination) #todaysdate
-        # except:
-        #         pass #folder already exist
-
-        # #Create subfolder with the time the files were transffered
-        # destination = destination + folderdatename + '/' + dt_string
-        # try:
-        #         os.mkdir(destination)
-        # except:
-        #         raise Exception('System cannot create a folder to move files')
-
         #Transform source string into path
         p = Path(source)
         #Read directory and move all folders inside it
         for f in p.iterdir():
-                if f.is_dir(): #if we want to copy folders only
-                        shutil.move(f, destination)
-                
+            if f.is_dir(): #if we want to copy folders only
+                # Use the os.path.join() function to construct the full paths of the source and destination directories
+                src_path = os.path.join(f, '')
+                dst_path = os.path.join(destination, os.path.basename(f))
+                # Move the directory from src_dir to dst_dir
+                move(src_path, dst_path)
     except Exception as e:
+        sys.stdout.write('faild: %s\n' % e)
+        traceback.print_exc()
         print('Something went wrong')
         print(e)
 
-#Printing current directory 
-directory = os.getcwd()
-print(directory)
-
-#Change the current working directory
-os.chdir('/Users/maccas/Documents/New Video Maker - Miller/MoviePyMiller_V1.4/VideoGenerator/data/samples/input/')
-
-#Printing current directory after the change
-directory = os.getcwd()
-print(directory)
-
 #Where the system will move the files from and to
+#PATH 1
 sourcesocialmedia = 'socialmediatemp/'
-destinationsocialmedia = '/Volumes/Media/Social Media/2022/3.TO BE ORGANISED/'
+destinationsocialmedia = '/Volumes/Media/Social Media/2023/3.TO BE ORGANISED/'
+#PATH 2
 sourceprocessed = 'processed/'
-destinationprocessed = '/Volumes/Customers/Automated-video-system-backup/'
+destinationprocessed = '/Volumes/Customers/Automated-video-system-backup-multistream/'
 
-#Running it to move files
+#Base folder to start copying files
+os.chdir('/Users/bigmac/Documents/GitHub/V152/VideoGenerator/data/samples/input/')
+
 MoveFiles(sourcesocialmedia, destinationsocialmedia)
 MoveFiles(sourceprocessed, destinationprocessed)
 
-#AUTO SCHEDULE WITH CRONTAB
+
+#INSTRUCTIONS TO USE AUTO SCHEDULE WITH CRONTAB (MACOS)
 #Open Terminal
 #Type: crontab -e
 #press i to start editing and add the following
-#0 17 * * * /usr/local/bin/python3 /Users/maccas/Documents/Moving\ Files\ to\ Server/MovingFilesPython/MoveFoldersToServer.py
+#50 7 * * * /usr/local/bin/python3 /Users/bigmac/Documents/GitHub/Moving\ Files\ to\ Server/MovingFilesPython/MoveFoldersToServer.py
 #press esc
 #type this to save :wq
 #########
 #crontab -l (Will list all scheduled jobs)
 #Here is a command that creates a text file to test it
-#37 10 * * * echo Miller text file > /Users/maccas/Documents/Moving\ Files\ to\ Server/MovingFilesPython/myfile.txt
-
+#37 10 * * * echo Miller text file > /Users/bigmac/Documents/GitHub/Moving\ Files\ to\ Server/MovingFilesPython/myfile.txt
+##############
+#Running Media Generator Automatically
+##0 8 * * * /usr/local/bin/python3 /Users/bigmac/Documents/GitHub/AutomaticVideoEditing/main.py 
